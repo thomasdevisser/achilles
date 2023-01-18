e842112559324413f2fa2a85d4abff3c19315bba
Thomas de Visser <thomasdevisser@pm.me>
2023-01-18 12:23:35 +0100

    Adding i18n through block patterns

---
+++ patterns/footer.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Title: Footer
+ * Slug: achilles/footer-pattern
+ */
+?>
+<!-- wp:paragraph {"align":"center"} -->
+  <p class="has-text-align-center"><?php _e( 'Copyright 2022 Achilles', 'achilles' ); ?></p>
+<!-- /wp:paragraph -->
